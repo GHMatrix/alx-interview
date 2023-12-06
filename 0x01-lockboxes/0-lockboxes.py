d38af13069a285ea8140f81cb8513275213669a7
GHMatrix <gsmensah@yahoo.com>
2023-12-06 17:14:26 +0000

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+Lockboses
+"""
+
+def canUnlockAll(boxes):
+    """
+    Determine if all the boxes can be opened.
+
+    Args:
+        boxes (List[List[int]]): A list of lists where each
+        inner list represents a box
+        and contains positive integers as keys.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+
+    Notes:
+        A key with the same number as a box opens that box.
+        The first box (boxes[0]) is unlocked.
+
+    Example:
+        >>> boxes = [[1], [2], [3], [4], []]
+        >>> canUnlockAll(boxes)
+        True
+
+        >>> boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+        >>> canUnlockAll(boxes)
+        True
+
+        >>> boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+        >>> canUnlockAll(boxes)
+        False
+    """
+    if not boxes or not boxes[0]:
+        return False
+
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    stack = [0]
+
+    while stack:
+        current_box = stack.pop()
+
+        for key in boxes[current_box]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                stack.append(key)
+
+    return all(visited)
