c623dae3b4edd5a587eca58feee4757649026e81
GHMatrix <gsmensah@yahoo.com>
2023-12-21 10:07:53 +0000

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,31 +1,34 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-Log parsing interview task
+Log parsing
 """
+
 
 import sys
 import signal
 
 
 def print_stats(total_size, status_codes):
-    print(f"File size: {total_size}")
+    print("File size: {}".format(total_size))
     for code in sorted(status_codes):
-        print(f"{code}: {status_codes[code]}")
+        print("{}: {}".format(code, status_codes[code]))
 
 
 def parse_line(line, total_size, status_codes):
     try:
         parts = line.split()
-        file_size, status_code = map(int, (parts[-1], parts[-2]))
+        file_size = int(parts[-1])
+        status_code = int(parts[-2])
         total_size += file_size
 
-        valid_status_codes = {200, 301, 400, 401, 403, 404, 405, 500}
-        if status_code in valid_status_codes:
-            status_codes[status_code] = status_codes.get(status_code, 0) + 1
+        if status_code in [200, 301, 400, 401, 403, 404, 405, 500]:
+            if status_code not in status_codes:
+                status_codes[status_code] = 1
+            else:
+                status_codes[status_code] += 1
 
         return total_size, status_codes
     except (ValueError, IndexError):
-        # Skip lines with incorrect format
         return total_size, status_codes
 
 
@@ -34,11 +37,8 @@ def main():
     status_codes = {}
     line_count = 0
 
-    def handle_interrupt(signum, frame):
-        print_stats(total_size, status_codes)
-        sys.exit(0)
-
-    signal.signal(signal.SIGINT, handle_interrupt)
+    signal.signal(signal.SIGINT, lambda signum, frame: print_stats(
+        total_size, status_codes) or sys.exit(0))
 
     for line in sys.stdin:
         line = line.strip()
