879cd68beaeb8ba916cf97fe5cba42c469480e86
GHMatrix <gsmensah@yahoo.com>
2023-12-21 10:21:29 +0000

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,55 +1,56 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 Log parsing interview challenge
 """
 
+
 import sys
 
-def parse_logs():
-    """
-    Reads logs from standard input and generates reports
-    Reports:
-        * Prints log size after reading every 10 lines & at KeyboardInterrupt
-    Raises:
-        KeyboardInterrupt (Exception): handles this exception and raises it
-    """
-    lineNumber = 0
-    fileSize = 0
-    statusCodes = {}
-    codes = ('200', '301', '400', '401', '403', '404', '405', '500')
-    
+
+def print_stats(total_size, status_codes):
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes.keys()):
+        print("{}: {}".format(code, status_codes[code]))
+
+
+def parse_line(line, total_size, status_codes):
+    try:
+        parts = line.split()
+        if len(parts) >= 7:
+            status_code = int(parts[-2])
+            file_size = int(parts[-1])
+            total_size += file_size
+
+            if status_code in {200, 301, 400, 401, 403, 404, 405, 500}:
+                if status_code not in status_codes:
+                    status_codes[status_code] = 0
+                status_codes[status_code] += 1
+
+        return total_size, status_codes
+    except ValueError:
+        return total_size, status_codes
+
+
+def main():
+    total_size = 0
+    status_codes = {}
+    line_count = 0
+
     try:
         for line in sys.stdin:
-            lineNumber += 1
-            parts = line.split()
+            line_count += 1
+            total_size, status_codes = parse_line(
+                    line, total_size, status_codes)
 
-            try:
-                fileSize += int(parts[-1])
-                if parts[-2] in codes:
-                    statusCodes[parts[-2]] = statusCodes.get(parts[-2], 0) + 1
-            except (IndexError, ValueError):
-                pass
-            
-            if lineNumber == 10:
-                report(fileSize, statusCodes)
-                lineNumber = 0
+            if line_count % 10 == 0:
+                print_stats(total_size, status_codes)
 
-        report(fileSize, statusCodes)
-        
     except KeyboardInterrupt:
-        report(fileSize, statusCodes)
-        raise
+        pass  # Handle keyboard interruption
 
-def report(fileSize, statusCodes):
-    """
-    Prints generated report to standard output
-    Args:
-        fileSize (int): total log size after every 10 successfully read lines
-        statusCodes (dict): dictionary of status codes and counts
-    """
-    print("File size: {}".format(fileSize))
-    for key, value in sorted(statusCodes.items()):
-        print("{}: {}".format(key, value))
+    finally:
+        print_stats(total_size, status_codes)
 
-if __name__ == '__main__':
-    parse_logs()
+
+if __name__ == "__main__":
+    main()
